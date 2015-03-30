788601426f705c27b970ce7f81642ff60ae2bdf9
ar1214 <ar1214git@gmail.com>
2015-03-30 00:49:01 -0500

    got restaurant and table models added and db schema working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,3 @@
 class Restaurant < ActiveRecord::Base
-has_many :tables
-
-
+  has_many :tables
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,7 +1,3 @@
 class Table < ActiveRecord::Base
-belongs_to :restaurant
-validates_presencePof :location
-validates_numericality_of :chairs
-
-
+  belongs_to :restaurant
 end
